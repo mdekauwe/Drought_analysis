17dbd17692dbff8b6ec41673621b713665431ffe
mdekauwe <mdekauwe@gmail.com>
2019-07-26 08:55:20 +1000

    Just take TVeg till I fix psi_soil output

--- generate_djf_nc_files.sh
+++ generate_djf_nc_files.sh
@@ -2,6 +2,9 @@
 
 cdo mergetime outputs/cable_out_*.nc outputs/all_yrs.nc
 
+cdo select,name=TVeg outputs/all_yrs.nc outputs/tmp_all_yrs.nc
+mv outputs/tmp_all_yrs.nc outputs/all_yrs.nc
+
 # Fix the longitude issue in the CABLE output files...
 cdo sellonlatbox,-180,180,-90,90 outputs/all_yrs.nc outputs/all_yrs_fixed_long.nc
 mv outputs/all_yrs_fixed_long.nc outputs/all_yrs.nc
