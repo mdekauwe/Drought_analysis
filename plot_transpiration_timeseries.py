5e4b5c25d3be3da591cfbbfe9132733725e942f3
mdekauwe <mdekauwe@gmail.com>
2019-09-06 20:09:07 +1000

    Added timeseries plot

--- plot_transpiration_timeseries.py
+++ plot_transpiration_timeseries.py
@@ -18,8 +18,8 @@ def main(fname_hyd):
     time = ds_ctl.time
     hyd = ds_hyd.TVeg
 
-    hyd19 = np.where(ds_iveg["iveg"] == 19, hyd, np.nan)
-    hyd20 = np.where(ds_iveg["iveg"] == 20, hyd, np.nan)
+    hyd19 = np.where(ds_hyd["iveg"] == 19, hyd, np.nan)
+    hyd20 = np.where(ds_hyd["iveg"] == 20, hyd, np.nan)
 
     hyd19 = np.nanmean(hyd19, axis=(1,2))
     hyd20 = np.nanmean(hyd20, axis=(1,2))
