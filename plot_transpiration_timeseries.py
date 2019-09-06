90f0d6ddc5a842cb195794cee33cb333274fe5b3
mdekauwe <mdekauwe@gmail.com>
2019-09-06 20:14:36 +1000

    Added timeseries plot

--- plot_transpiration_timeseries.py
+++ plot_transpiration_timeseries.py
@@ -15,12 +15,11 @@ import sys
 def main(fname_hyd, plot_dir):
 
     ds_hyd = xr.open_dataset(fname_hyd)
-    ds_iveg = xr.open_dataset(fname_iveg)
     time = ds_hyd.time
     hyd = ds_hyd.TVeg
 
-    hyd19 = np.where(ds_iveg["iveg"] == 19, hyd, np.nan)
-    hyd20 = np.where(ds_iveg["iveg"] == 20, hyd, np.nan)
+    hyd19 = np.where(ds_hyd["iveg"] == 19, hyd, np.nan)
+    hyd20 = np.where(ds_hyd["iveg"] == 20, hyd, np.nan)
 
     hyd19 = np.nanmean(hyd19, axis=(1,2))
     hyd20 = np.nanmean(hyd20, axis=(1,2))
