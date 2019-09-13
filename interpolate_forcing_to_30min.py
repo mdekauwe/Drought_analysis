28e5d59c04e7b027f961f7b2eba8566e83ac3367
mdekauwe <mdekauwe@gmail.com>
2019-09-13 15:00:49 +1000

    bones, bones, bones

--- interpolate_forcing_to_30min.py
+++ interpolate_forcing_to_30min.py
@@ -11,26 +11,73 @@ import xarray as xr
 import numpy as np
 import sys
 import glob
+import subprocess
 
 def interpolate_forcing(fpath, var, output_dir):
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    output_dir_var = os.path.join(output_dir, var)
+    if not os.path.exists(output_dir_var):
+        os.makedirs(output_dir_var)
+
     files = glob.glob(os.path.join(fpath, "%s/*.nc") % (var))
     years = np.sort(np.asarray([int(f[-7:-3]) for f in files]))
 
     last_year = years[-1]
     start_date = "%d-01-01,00:00:00" % (years[0])
-    
+
     for year in years:
 
-        fn = os.path.join(fpath, "GSWP3.BC.%s.3hrMap.%d.nc" % (var, year))
+        fn = os.path.join(fpath,
+                          "%s/GSWP3.BC.%s.3hrMap.%d.nc" % (var, var, year))
 
         if year != last_year:
             print(fn)
 
+            nxt_fn = os.path.join(output_dir_var,
+                                  "GSWP3.BC.%s.3hrMap.%d.nc" % (var, year + 1))
+            start_date = "%d-01-01,00:00:00" % (year)
 
+            cmd = "cdo inttime,%s,30minutes %s %s" % (start_date, fn, nxt_fn)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                raise Exception("Error interpolating file")
+        else:
+
+            # Get the last day
+            last_date = "%d-12-31,00:00:00" % (year)
+            tmp_fn = os.path.join(output_dir_var, "tmp.nc")
+            cmd = "cdo seldate,%s %s %s" % (last_date, fn, tmp_fn)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                raise Exception("Error getting the last day")
+
+            # change the date
+            tmp2_fn = os.path.join(output_dir_var, "tmp2.nc")
+            new_date = "%d-01-01,00:00:00" % (year + 1)
+            cmd = "cdo settaxis,%s,3hours %s %s" % (new_date, tmp_fn, tmp2_fn)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                raise Exception("Error changing the date file")
+
+            # merge dummy date
+            tmp3_fn = os.path.join(output_dir_var, "tmp3.nc")
+            cmd = "cdo mergetime %s %s %s" (fn, tmp2_fn, tmp3_fn)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                raise Exception("Error merging new file")
+
+            nxt_fn = os.path.join(output_dir_var,
+                                  "GSWP3.BC.%s.3hrMap.%d.nc" % (var, year + 1))
+            start_date = "%d-01-01,00:00:00" % (year)
+
+            cmd = "cdo inttime,%s,30minutes %s %s" % (start_date, fn, nxt_fn)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                raise Exception("Error interpolating file")
+                
     #for
     #print(files)
 
