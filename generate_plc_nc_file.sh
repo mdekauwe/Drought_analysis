aec28383bf5ec0324915e24c69309bd6bf380b99
mdekauwe <mdekauwe@gmail.com>
2019-09-23 19:32:28 +1000

    Added script to get min plc

---
+++ generate_plc_nc_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cdo mergetime outputs/cable_out_*.nc outputs/all_yrs_plc.nc
+
+cdo select,name=plc outputs/all_yrs_plc.nc outputs/tmp_all_yrs.nc
+mv outputs/tmp_all_yrs.nc outputs/all_yrs_plc.nc
+
+# Fix the longitude issue in the CABLE output files...
+cdo sellonlatbox,-180,180,-90,90 outputs/all_yrs_plc.nc outputs/all_yrs_fixed_long.nc
+mv outputs/all_yrs_fixed_long.nc outputs/all_yrs_plc.nc
+
+# get minimum PLC
+cdo min outputs/all_yrs_plc.nc outputs/min_plc.nc
