69819dfa1501edf2fa62a12ba6235ec7f935f8c6
mdekauwe <mdekauwe@gmail.com>
2019-09-26 13:45:53 +1000

    Tweaked plot

--- plot_CMI.py
+++ plot_CMI.py
@@ -49,7 +49,7 @@ def main(fname, plot_dir):
     plt.rcParams['font.size'] = "14"
     plt.rcParams['font.sans-serif'] = "Helvetica"
 
-    cmap = plt.cm.get_cmap('BrBG', 5) # discrete colour map
+    cmap = plt.cm.get_cmap('BrBG', 6) # discrete colour map
 
     projection = ccrs.PlateCarree()
     axes_class = (GeoAxes, dict(map_projection=projection))
@@ -82,7 +82,7 @@ def main(fname, plot_dir):
 
 def plot_map(ax, var, cmap, i):
     print(np.nanmin(var), np.nanmax(var))
-    vmin, vmax = -600, 600
+    vmin, vmax = -600, 300
     top, bottom = 90, -90
     left, right = -180, 180
     img = ax.imshow(var, origin='lower',
