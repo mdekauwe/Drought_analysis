dd89a38b7b8c1b183a831262571d1281ae3076f9
mdekauwe <mdekauwe@gmail.com>
2019-10-02 09:56:06 +1000

    Fixed lons, files

--- generate_plc_nc_file_AWAP.sh
+++ generate_plc_nc_file_AWAP.sh
@@ -6,5 +6,5 @@ cdo select,name=plc outputs/all_yrs_plc.nc outputs/tmp_all_yrs.nc
 mv outputs/tmp_all_yrs.nc outputs/all_yrs_plc.nc
 
 # Fix the longitude issue in the CABLE output files...
-cdo sellonlatbox,-180,180,-90,90 outputs/all_yrs_plc.nc outputs/all_yrs_fixed_long.nc
+cdo sellonlatbox,112,154,-44,-10 outputs/all_yrs_plc.nc outputs/all_yrs_fixed_long.nc
 mv outputs/all_yrs_fixed_long.nc outputs/all_yrs_plc.nc
