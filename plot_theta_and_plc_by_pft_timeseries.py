2f4f8c8a8c6efe0688afd88af576be1b774cab4b
mdekauwe <mdekauwe@gmail.com>
2019-11-11 23:24:23 +1100

    Added spread

--- plot_theta_and_plc_by_pft_timeseries.py
+++ plot_theta_and_plc_by_pft_timeseries.py
@@ -205,16 +205,16 @@ def main(plot_dir):
     ax.plot(dates, plc_grw_all, label="GRW", color=colours[3], lw=2)
     ax.plot(dates, plc_saw_all, label="SAW", color=colours[0], lw=2)
 
-    ax.fill_between(dates, plc_rf_all+plc_rf_sigma, plc_rf_all-plc_rf_sigma,
-                    facecolor=colours[6], alpha=0.5)
-    ax.fill_between(dates, plc_wsf_all+plc_wsf_sigma, plc_wsf_all-plc_wsf_sigma,
-                    facecolor=colours[1], alpha=0.5)
-    ax.fill_between(dates, plc_dsf_all+plc_dsf_sigma, plc_dsf_all-plc_dsf_sigma,
-                    facecolor=colours[2], alpha=0.5)
-    ax.fill_between(dates, plc_grw_all+plc_grw_sigma, plc_grw_all-plc_grw_sigma,
-                    facecolor=colours[3], alpha=0.5)
-    ax.fill_between(dates, plc_saw_all+plc_saw_sigma, plc_saw_all-plc_saw_sigma,
-                    facecolor=colours[0], alpha=0.5)
+    #ax.fill_between(dates, plc_rf_all+plc_rf_sigma, plc_rf_all-plc_rf_sigma,
+    #                facecolor=colours[6], alpha=0.5)
+    #ax.fill_between(dates, plc_wsf_all+plc_wsf_sigma, plc_wsf_all-plc_wsf_sigma,
+    #                facecolor=colours[1], alpha=0.5)
+    #ax.fill_between(dates, plc_dsf_all+plc_dsf_sigma, plc_dsf_all-plc_dsf_sigma,
+    #                facecolor=colours[2], alpha=0.5)
+    #ax.fill_between(dates, plc_grw_all+plc_grw_sigma, plc_grw_all-plc_grw_sigma,
+    #                facecolor=colours[3], alpha=0.5)
+    #ax.fill_between(dates, plc_saw_all+plc_saw_sigma, plc_saw_all-plc_saw_sigma,
+    #                facecolor=colours[0], alpha=0.5)
 
 
     ax.axhline(y=88.0, ls="--", lw=2, color="black", label="$\Psi$$_{crit}$")
