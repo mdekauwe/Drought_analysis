cb446b672c13efa5915b1c84ca7bf05f12758e36
mdekauwe <mdekauwe@gmail.com>
2019-11-25 15:16:02 +1100

    Added LAI stuff

--- generate_djf_nc_files_AWAP.sh
+++ generate_djf_nc_files_AWAP.sh
@@ -15,4 +15,13 @@ mv outputs/all_yrs_fixed_long.nc outputs/all_yrs.nc
 #9 skip 9 months between every 3 months interval (March to November)
 cdo mulc,86400 -timselmean,3,11,9 outputs/all_yrs.nc outputs/djf.nc
 
+
+cdo mergetime outputs/cable_out_*.nc outputs/all_yrs.nc
+cdo select,name=TVeg,LAI,GPP outputs/all_yrs.nc outputs/tmp_all_yrs.nc
+mv outputs/tmp_all_yrs.nc outputs/all_yrs.nc
+
+# Fix the longitude issue in the CABLE output files...
+cdo sellonlatbox,112,154,-44,-10 outputs/all_yrs.nc outputs/all_yrs_fixed_long.nc
+mv outputs/all_yrs_fixed_long.nc outputs/all_yrs.nc
+
 cdo mulc,86400 -timselmean,3,11,9 outputs/all_yrs.nc outputs/djf_lai.nc
