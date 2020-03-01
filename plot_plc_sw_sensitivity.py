bab4ff4a90a47e3a8af8d5b7f882f2a69b40ec1b
mdekauwe <mdekauwe@gmail.com>
2020-03-02 09:39:38 +1100

    cleaned up plot

--- plot_plc_sw_sensitivity.py
+++ plot_plc_sw_sensitivity.py
@@ -170,15 +170,15 @@ def main(plot_dir):
     ax2.scatter(sw_wsf_all, plc_wsf_all, marker=".",
              alpha=0.05, color=colours[1], rasterized=True)
 
-    ax3.scatter(sw_dsf_all[:,i], plc_dsf_all[:,i], marker=".",
+    ax3.scatter(sw_dsf_all[:,i], plc_dsf_all, marker=".",
                 alpha=0.05, color=colours[2], rasterized=True)
 
-    ax4.scatter(sw_grw_all[:,i], plc_grw_all[:,i], marker=".",
+    ax4.scatter(sw_grw_all[:,i], plc_grw_all, marker=".",
                 alpha=0.05, color=colours[3], rasterized=True)
 
-    ax5.scatter(sw_saw_all[:,i], plc_saw_all[:,i], marker=".",
+    ax5.scatter(sw_saw_all[:,i], plc_saw_all, marker=".",
                 alpha=0.05, color=colours[4], rasterized=True)
-    
+
 
     ax1.set_ylim(-5, 90)
     ax2.set_ylim(-5, 90)
