1357188d3874419142afdcf4c60a4259944b677c
mdekauwe <mdekauwe@gmail.com>
2020-03-03 12:16:27 +1100

    Plot tweaks

--- plot_plc_sw_sensitivity.py
+++ plot_plc_sw_sensitivity.py
@@ -20,6 +20,7 @@ from cartopy.mpl.geoaxes import GeoAxes
 from mpl_toolkits.axes_grid1 import AxesGrid
 from calendar import monthrange
 import pandas as pd
+import string
 
 def main(plot_dir):
 
@@ -148,70 +149,75 @@ def main(plot_dir):
     #colours = cm.Set2(np.linspace(0, 1, 5))
     #colours = cm.get_cmap('Set2')
 
+    labels = label_generator('lower', start="(", end=")")
 
     import seaborn as sns
     sns.set_style("ticks")
     colours = sns.color_palette("Set2", 8)
 
 
-    fig = plt.figure(figsize=(6,9))
+    fig = plt.figure(figsize=(20,9))
     fig.subplots_adjust(hspace=0.1)
-    fig.subplots_adjust(wspace=0.05)
+    fig.subplots_adjust(wspace=0.1)
     plt.rcParams['text.usetex'] = False
     plt.rcParams['font.family'] = "sans-serif"
     plt.rcParams['font.sans-serif'] = "Helvetica"
 
     plt.rcParams['axes.labelsize'] = 14
     plt.rcParams['font.size'] = 14
-    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['legend.fontsize'] = 14
     plt.rcParams['xtick.labelsize'] = 14
     plt.rcParams['ytick.labelsize'] = 14
 
-    ax1 = fig.add_subplot(511)
-    ax2 = fig.add_subplot(512)
-    ax3 = fig.add_subplot(513)
-    ax4 = fig.add_subplot(514)
-    ax5 = fig.add_subplot(515)
+    ax1 = fig.add_subplot(231)
+    ax2 = fig.add_subplot(232)
+    ax3 = fig.add_subplot(233)
+    ax4 = fig.add_subplot(234)
+    ax5 = fig.add_subplot(235)
 
-    ax1.scatter(sw_rf_all, plc_rf_all,  marker=".", s=0.01,
-             alpha=0.05, color=colours[0], rasterized=True,
-             label="RF")
+    ax1.scatter(sw_rf_all, plc_rf_all,  marker=".", s=0.05,
+             alpha=0.05, color=colours[0], rasterized=True)
+    ax2.scatter(sw_wsf_all, plc_wsf_all, marker=".", s=0.05,
+             alpha=0.05, color=colours[0], rasterized=True)
+    ax3.scatter(sw_dsf_all, plc_dsf_all, marker=".", s=0.05,
+                alpha=0.05, color=colours[0], rasterized=True)
+    ax4.scatter(sw_grw_all, plc_grw_all, marker=".", s=0.05,
+                alpha=0.05, color=colours[0], rasterized=True)
+    ax5.scatter(sw_saw_all, plc_saw_all, marker=".", s=0.05,
+                alpha=0.05, color=colours[0], rasterized=True)
 
-    #from scipy.stats import gaussian_kde
-    #
-    #x = sw_wsf_all
-    #y = plc_wsf_all
-    #x = x[y>0.0]
-    #y = y[y>0.0]
-    #xy = np.vstack([x,y])
-    #z = gaussian_kde(xy)(xy)
-    #ax2.scatter(x, y, c=z, s=100, edgecolor='')
-    ax2.scatter(sw_wsf_all, plc_wsf_all, marker=".", s=0.01,
-             alpha=0.05, color=colours[1], rasterized=True,
-             label="WSF")
+    props = dict(boxstyle='round', facecolor='white', alpha=1.0,
+                 ec="white")
 
-    ax3.scatter(sw_dsf_all, plc_dsf_all, marker=".", s=0.01,
-                alpha=0.05, color=colours[2], rasterized=True,
-                label="DSF")
-    ax4.scatter(sw_grw_all, plc_grw_all, marker=".", s=0.01,
-                alpha=0.05, color=colours[3], rasterized=True,
-                label="GRW")
+    fig_label = "%s %s" % (next(labels), "RF")
+    ax1.text(0.8, 0.8, fig_label,
+            transform=ax1.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels), "WSF")
+    ax2.text(0.8, 0.8, fig_label,
+            transform=ax2.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels), "DSF")
+    ax3.text(0.8, 0.8, fig_label,
+            transform=ax3.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels), "GRW")
+    ax4.text(0.8, 0.8, fig_label,
+            transform=ax4.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels), "SAW")
+    ax5.text(0.8, 0.8, fig_label,
+            transform=ax5.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
 
-    ax5.scatter(sw_saw_all, plc_saw_all, marker=".", s=0.01,
-                alpha=0.05, color=colours[4], rasterized=True,
-                label="SAW")
 
-    ax1.axhline(y=88.0, ls="--", lw=1, color="lightgrey", label="$\Psi$$_{crit}$")
-    ax2.axhline(y=88.0, ls="--", lw=1, color="lightgrey")
-    ax3.axhline(y=88.0, ls="--", lw=1, color="lightgrey")
-    ax4.axhline(y=88.0, ls="--", lw=1, color="lightgrey")
-    ax5.axhline(y=88.0, ls="--", lw=1, color="lightgrey")
+    ax1.axhline(y=88.0, ls="--", lw=1, color="black", label="$\Psi$$_{crit}$")
+    ax2.axhline(y=88.0, ls="--", lw=1, color="black")
+    ax3.axhline(y=88.0, ls="--", lw=1, color="black")
+    ax4.axhline(y=88.0, ls="--", lw=1, color="black")
+    ax5.axhline(y=88.0, ls="--", lw=1, color="black")
 
-    ax1.legend(numpoints=1, loc=(0.75, 0.45), ncol=1, frameon=False)
-    ax2.legend(numpoints=1, loc=(0.75, 0.45), ncol=1, frameon=False)
-    ax3.legend(numpoints=1, loc=(0.75, 0.45), ncol=1, frameon=False)
-    ax4.legend(numpoints=1, loc=(0.75, 0.45), ncol=1, frameon=False)
-    ax5.legend(numpoints=1, loc=(0.75, 0.45), ncol=1, frameon=False)
+    ax1.legend(numpoints=1, loc=(0.75, 0.6), ncol=1, frameon=False)
 
 
     ax1.set_ylim(-5, 100)
@@ -226,28 +232,48 @@ def main(plot_dir):
     ax4.set_xlim(0.1, 0.3)
     ax5.set_xlim(0.1, 0.3)
 
-    ax3.set_ylabel("Loss of hydraulic\nconductivity (%)")
+    ax1.set_ylabel("Loss of hydraulic conductivity (%)", position=(0.5, 0.0))
     ax5.set_xlabel(r"$\theta$ (m$^{3}$ m$^{-3}$)")
     #ax.legend(numpoints=1, loc=(0.01, 0.65), ncol=1, frameon=False)
 
 
     plt.setp(ax1.get_xticklabels(), visible=False)
     plt.setp(ax2.get_xticklabels(), visible=False)
-    plt.setp(ax3.get_xticklabels(), visible=False)
-    plt.setp(ax4.get_xticklabels(), visible=False)
 
-    odir = "plots"
-    plt.savefig(os.path.join(odir, "plc_vs_sw_all.png"), dpi=150,
+    plt.setp(ax2.get_yticklabels(), visible=False)
+    plt.setp(ax3.get_yticklabels(), visible=False)
+    plt.setp(ax5.get_yticklabels(), visible=False)
+
+    from matplotlib.ticker import MaxNLocator
+    ax1.xaxis.set_major_locator(MaxNLocator(4))
+    ax2.xaxis.set_major_locator(MaxNLocator(4))
+    ax3.xaxis.set_major_locator(MaxNLocator(4))
+    ax4.xaxis.set_major_locator(MaxNLocator(4))
+    ax5.xaxis.set_major_locator(MaxNLocator(4))
+
+    ax1.yaxis.set_major_locator(MaxNLocator(4))
+    ax2.yaxis.set_major_locator(MaxNLocator(4))
+    ax3.yaxis.set_major_locator(MaxNLocator(4))
+    ax4.yaxis.set_major_locator(MaxNLocator(4))
+    ax5.xaxis.set_major_locator(MaxNLocator(4))
+
+    plt.savefig(os.path.join(plot_dir, "plc_vs_sw_all.png"), dpi=300,
                 bbox_inches='tight', pad_inches=0.1)
 
     #plt.show()
 
 
+def label_generator(case='lower', start='', end=''):
+    choose_type = {'lower': string.ascii_lowercase,
+                   'upper': string.ascii_uppercase}
+    generator = ('%s%s%s' %(start, letter, end) for letter in choose_type[case])
 
+    return generator
 
 if __name__ == "__main__":
 
-    plot_dir = "plots"
+    #plot_dir = "plots"
+    plot_dir = "/Users/mdekauwe/Dropbox/Drought_risk_paper/figures/figs"
     if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
 
